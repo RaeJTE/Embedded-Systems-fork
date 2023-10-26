506af533a00a9b44a921c61f6776dfbfaf53c282
RaeJTE <jacob.rae@students.plymouth.ac.uk>
2023-10-26 12:11:57 +0100

    Task 126 completed.

--- Tasks/Task-126-MyAttempt/main.cpp
+++ Tasks/Task-126-MyAttempt/main.cpp
@@ -1,14 +1,16 @@
 #include "mbed.h"
 
 DigitalIn ButtonA(PG_0); //Button A
-DigitalIn ButtonB(PG_1); //Button B
+DigitalIn ButtonB(PG_1); //Button B 
+DigitalIn ButtonC(PG_2, PinMode :: PullDown); //Button C
+DigitalIn ButtonD(PG_3, PinMode :: PullDown); //Button D
 
 DigitalOut redLED(PC_2); //Red Traffic 1
 
 // main() runs in its own thread in the OS
 int main()
 {
-    int btnA, btnB;
+    int btnA, btnB, btnC, btnD;
     // Turn OFF the red LED
     redLED = 0;
 
@@ -18,27 +20,36 @@ int main()
         do {
             btnA = ButtonA; //Read button A
             btnB = ButtonB; //Read button B
-        } while ((btnA == 0) || (btnB == 0));
+            btnC = ButtonC;
+            btnD = ButtonD;
+        } while ((btnA == 0) || (btnB == 0) || (btnC == 0) || (btnD == 0));
 
         //Toggle the red LED
         redLED = !redLED;
 
-        printf("\n");
+        printf("-------------\n");
 
         //Wait for noise to settle
         wait_us(10000);
 
         printf("Button A is set to %d \n", btnA);
         printf("Button B is set to %d \n", btnB);
+        printf("Button C is set to %d \n", btnC);
+        printf("Button D is set to %d \n", btnD);
+        printf("\n");
 
         // Wait for EITHER buttons to be released
         do {
             btnA = ButtonA; //Read button A
             btnB = ButtonB; //Read button B
-        } while ((btnA == 1) && (btnB == 1));
+            btnC = ButtonC;
+            btnD = ButtonD;
+        } while ((btnA + btnB + btnC + btnD) >= 3);
 
         printf("Button A is set to %d \n", btnA);
         printf("Button B is set to %d \n", btnB);
+        printf("Button C is set to %d \n", btnC);
+        printf("Button D is set to %d \n", btnD);
 
         //Wait for noise to settle
         wait_us(10000);

f0223dfa9d1828b70504a8e9f2ca4fa570f5104f
RaeJTE <jacob.rae@students.plymouth.ac.uk>
2023-11-09 12:30:37 +0000

    Task 156 mostly complete, some problems with the Advanced/Challenge portion.

--- Tasks/Task-156-Advanced/main.cpp
+++ Tasks/Task-156-Advanced/main.cpp
@@ -14,46 +14,50 @@ AnalogIn ldr(AN_LDR_PIN);
 Buzzer buzz;
 
 
+/*float Wrap(int kX, int const kLowerBound, int const kUpperBound) {
+            int range_size = kUpperBound - kLowerBound + 1;
+
+            if (kX < kLowerBound)
+                kX += range_size * ((kLowerBound - kX) / range_size + 1);
+
+            return kLowerBound + (kX - kLowerBound) % range_size;
+        }
+        
+        This function is no longer needed, but it was cool so I wanted to keep it recorded
+        Its purpose it to let you make a range over which numbers will wrap back around - could implement with button counter from Task-134 */
+
+
 int main()
 {
     float samples[100] = {0};
     int size = *(&samples + 1) - samples; //This gives the length of the array, by using the fact that a pointer +1 will skip to the memory location just after the end of the array and that subtract the position of the array start is the legnth of the array
     printf("n is %i \n", size);
-
-
-    for (unsigned int m=0; m<100; m++) {
-        printf("Sample number: %i is %f \n", m, samples[m]);
-    }
+    float mean = 0;
 
     // Automatic headlamp 
     while (true) {
 
         float sum = 0;
 
+
+
         for (unsigned int m=0; m<100; m++) {
             unsigned short ldrVal   = ldr.read_u16();
+            sum -= samples [m]; //removes oldest value
             samples[m] = ldrVal;
-            sum += samples[m];
-            wait_us(10000);      // 10ms
-        }
-
-        // TASK a. Calculate the average value in samples
-        float mean = 0;
-        for (unsigned int m=0; m<100; m++) {
+            sum += samples[m]; //adds newest value
             mean = sum/size;
-        }
-        printf("The mean of the samples is: %f \n", mean); //seem to get 4,670 +/- 50 when uncovered; 23,800 +/- 1,000 when covered.
 
-        // TASK b. Display to 1dp
-        printf("The mean to 1dp is: %0.1f \n", mean);
-
-        // TASK c. Switch green LED on when dark;
-        if (mean > 23000) {
+            if (mean > 23000) {
             redLED = 1;
-        }
-        else if (mean < 5000) {
+            }
+            else if (mean < 5000) {
             redLED = 0;
+            }
+            wait_us(10000);      // 10ms
+            printf("The mean to 1dp is: %0.1f \n", mean);
         }
+
 
     }  
 }

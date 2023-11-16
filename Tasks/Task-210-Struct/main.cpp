16bf296fb901df38765d01e7e2179ceaae1f4e86
RaeJTE <jacob.rae@students.plymouth.ac.uk>
2023-11-16 13:02:07 +0000

    Task 210 completed.

--- Tasks/Task-210-Struct/main.cpp
+++ Tasks/Task-210-Struct/main.cpp
@@ -21,8 +21,12 @@ int main() {
 
     // TASK:
     // Create another complex number y
+    ComplexNumber_C y;
     // Calculate the complex sum of p and q and store in y
+    y.real = p.real + q.real;
+    y.imag = p.imag + q.imag;
     // Use printf to display as a complex number (hint: you need two placeholders)
+    printf("The value of y is %.1f + j%.1f\n", y.real, y.imag);
     
     while (true) {
     }

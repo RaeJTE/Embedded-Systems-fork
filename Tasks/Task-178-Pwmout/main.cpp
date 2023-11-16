e2c5e7788b1b5d8a4af4a9fb51a7e8e8158693ab
RaeJTE <jacob.rae@students.plymouth.ac.uk>
2023-11-16 12:31:38 +0000

    Task 178 completed.

--- Tasks/Task-178-Pwmout/main.cpp
+++ Tasks/Task-178-Pwmout/main.cpp
@@ -4,6 +4,7 @@ using namespace uop_msb;
 extern int getAverageDelay(double alpha);
 
 AnalogIn pot(AN_POT_PIN);
+AnalogIn LDR(AN_LDR_PIN);
 DigitalOut redLED(TRAF_RED1_PIN);
 LCD_16X2_DISPLAY disp;
 PwmOut dispBackLight(LCD_BKL_PIN);
@@ -30,15 +31,36 @@ int main()
 
     //Timer for the pot
     Timer tmr;
+    Timer tmr2;
     tmr.start();
+    tmr2.start();
 
     while(true) {
         
-        //Once every 100ms, re-read the POT and update the duty
+        //Once every 250ms, re-read the POT and update the duty
         if (tmr.elapsed_time() >= 250ms) {
             float u = pot;              // Every 250ms, read the pot... 
             dispBackLight.write(u);     // ... and update the brightness.
             tmr.reset();
+            if (u >= 0.5) {
+                redLED = 1;
+            }
+            else {
+                redLED = 0;
+            }
+        }
+        if (tmr2.elapsed_time() >= 1s) {
+            float u = pot;
+            float v = LDR;
+            
+            disp.locate(0, 0);
+            disp.cls();
+            disp.printf("Pot = %0.5f", u);
+
+            disp.locate(1, 0);
+            disp.printf("LDR = %0.5f", v);
+
+            tmr2.reset();
         }
         
         //Other non-blocking code could go here

1131282cd997ab55d9a9adad1d97c32b31b27dfa
RaeJTE <147714975+RaeJTE@users.noreply.github.com>
2025-10-03 15:47:40 +0100

    Task 206 and others completed
    ELEC 351 week 2 tasks completed (some already were)

--- Tasks/Task-207-Timer/main.cpp
+++ Tasks/Task-207-Timer/main.cpp
@@ -17,7 +17,7 @@ int main()
 {
     //Time how long it takes to perform a printf
     tmr1.start();
-    printf("Hello World!\n");
+    printf("Hello World How Are You!\n");
     tmr1.stop();
 
     //Print out how long it took
@@ -28,21 +28,15 @@ int main()
     tmr1.start();
     while (true) {
         //Wait for switch press
-        while (SW_BLUE == 0);
-
-        //Turn on LED
-        ledRed = 1;
-
-        //Wait for 500ms
-        tmr1.reset();
-        while (tmr1.elapsed_time() < 500ms); //How about this for C++ magic :)
-
-        wait_us(500000);
-
-        //Turn off LED
-        ledRed = 0;
+        while (SW_BLUE == 0)
+        {
+            wait_us(1000);
+        };
+        while (SW_BLUE == 1)
+        {
+            wait_us(1000);
+        };
+        //Toggle LED
+        ledRed = !ledRed;
     }
 }
-
-
-
